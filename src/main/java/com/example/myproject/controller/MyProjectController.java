5cb3efb274e3b4bc2259dc669f595662092df339
luminousol <luminousolding@gmail.com>
2023-07-27 15:23:50 +0900

    first commit

---
+++ src/main/java/com/example/myproject/controller/MyProjectController.java
@@ -0,0 +1,16 @@
+package com.example.myproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MyProjectController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+
+    }
+}
